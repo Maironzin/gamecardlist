f4b04107280c70abf20bb06896114d892b8e7256
Maironzin <patrick.mairon.jl@gmail.com>
2026-04-25 15:15:58 -0300

    commit versão 2.0
    Agora da pra exportar um json e salvar as alterações feitas;
    Novas telas para configurações proprias;

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "280824950"
+    serviceWorkerVersion: "1107446070"
   }
 });
